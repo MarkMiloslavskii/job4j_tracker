f1ee0e3f7f49c064af4a6d3b08d72fb5295087e8
ivanvelikij <eldarvaleev01@gmail.com>
2024-10-04 10:08:11 +0300

    1.8. Класс использующий сам себя [#167388 #515796]

--- src/test/java/ru/job4j/tracker/BatteryTest.java
+++ src/test/java/ru/job4j/tracker/BatteryTest.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 import ru.job4j.oop.Battery;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BatteryTest {
