89bbe5537f2b6d8d6dced06b632265c57b4d91ed
ivanvelikij <eldarvaleev01@gmail.com>
2024-10-08 09:26:55 +0300

    8. Date. Отображение даты. [#310017 #516289]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,7 +5,10 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
-    private LocalDateTime created;
+    private final LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+    }
 
     public Item(String name) {
         this.name = name;
@@ -14,10 +17,6 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public Item() {
-        this.created = LocalDateTime.now();
     }
 
     public void printInfo() {
