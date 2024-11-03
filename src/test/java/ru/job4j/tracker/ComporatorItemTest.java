04cd177037ae7cd39778aea718dbc69a818c3e0d
ivanvelikij <eldarvaleev01@gmail.com>
2024-11-03 22:51:21 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ComporatorItemTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.tracker;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ComporatorItemTest {
+    @Test
+            void SortItems() {
+        List<Item> items = Arrays.asList(
+                new Item("one"),
+                new Item("two"),
+                new Item("three"),
+                new Item("four"),
+                new Item("five")
+        );
+        Collections.sort(items, new ItemAscByName());
+        List<Item> expected = Arrays.asList(
+                new Item("five"),
+                new Item("four"),
+                new Item("one"),
+                new Item("three"),
+                new Item("two")
+        );
+    Assert.assertEquals(items, expected);
+    }
+
+    @Test
+    void SortDiscItems() {
+        List<Item> items = Arrays.asList(
+                new Item("one"),
+                new Item("two"),
+                new Item("three"),
+                new Item("four"),
+                new Item("five")
+        );
+        Collections.sort(items, new ItemDescByName());
+        List<Item> expected = Arrays.asList(
+                new Item("two"),
+                new Item("three"),
+                new Item("one"),
+                new Item("four"),
+                new Item("five")
+        );
+        Assert.assertEquals(items, expected);
+    }
+}
