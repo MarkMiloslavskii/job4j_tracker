6a7d6354d3b5f0027b9b8d9358be03f7b70085d2
ivanvelikij <eldarvaleev01@gmail.com>
2024-11-10 18:22:45 +0300

    2.2. Функциональный интерфейс [#247497]

---
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -0,0 +1,46 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.*;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biConsumer = (key, value) -> map.put(key, value);
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+        map.put(5, "five");
+        map.put(6, "six");
+        map.put(7, "seven");
+        map.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        BiPredicate<Integer, String> biPredicate = (key, value) -> key % 2 == 0 || map.get(key).length() == 4;
+        for (Integer key : map.keySet()) {
+            String value = map.get(key);
+            if (biPredicate.test(key, value)) {
+                System.out.println(key + " " + value);
+            }
+        }
+        /*
+            Заменить создание ArrayList из значений Map на Supplier, объявлен ниже, требуется его реализовать.
+         */
+        Supplier<List<String>> supplier = () -> new ArrayList<>(map.values());
+        List<String> strings = supplier.get();
+
+        Consumer<String> consumer = value -> System.out.println(value);
+        Function<String, String> function = value -> value.toUpperCase();
+        for (String string : strings) {
+            /*
+                Заменить вывод строк на применение Consumer
+                Заменить преобразование строк к строкам в верхнем регистре с помощью Function
+                Необходимое объявлено выше, требуется их реализовать.
+            */
+            System.out.println(string.toUpperCase());
+        }
+    }
+}
